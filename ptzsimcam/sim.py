de71398f46eeed921b67bd1299c071f07894f233
Maxime Desmarais <contact@mdesmarais.fr>
2021-11-02 11:48:53 +0100

    Add sliders for testing velocity

--- ptzsimcam/sim.py
+++ ptzsimcam/sim.py
@@ -1,6 +1,7 @@
 import pybullet as pb
 
-from camera import Camera
+from camera import Camera, handle_pan_tilt_packet
+from visca import RawViscaPacket
 
 
 class Simulation:
@@ -9,15 +10,39 @@ class Simulation:
         self.camera = camera
         self.debug = debug
 
-        """self.pan_slider = pb.addUserDebugParameter('PAN', -100, 100, startValue=0)
+        #self.pan_slider = pb.addUserDebugParameter('PAN', -100, 100, startValue=0)
         self.pan_vel_slider = pb.addUserDebugParameter('PAN vel', -0x18, 0x18, startValue=0)
 
-        self.tilt_slider = pb.addUserDebugParameter('TILT', -25, 25, startValue=0)
-        self.tilt_vel_slider = pb.addUserDebugParameter('TILT vel', -0x17, 0x17, startValue=0)"""
+        #self.tilt_slider = pb.addUserDebugParameter('TILT', -25, 25, startValue=0)
+        self.tilt_vel_slider = pb.addUserDebugParameter('TILT vel', -0x17, 0x17, startValue=0)
 
     def run(self):
         while True:
-            pass
+            pan_vel = pb.readUserDebugParameter(self.pan_vel_slider)
+            tilt_vel = pb.readUserDebugParameter(self.tilt_vel_slider)
+
+            if pan_vel < 0:
+                pan_dir = 1
+            elif pan_vel > 0:
+                pan_dir = 2
+            else:
+                pan_dir = 3
+
+            if tilt_vel < 0:
+                tilt_dir = 1
+            elif tilt_vel > 0:
+                tilt_dir = 2
+            else:
+                tilt_dir = 3
+
+            data = bytearray(b'\x01\x06\x01')
+            data.append(abs(int(pan_vel)))
+            data.append(abs(int(tilt_vel)))
+            data.append(pan_dir)
+            data.append(tilt_dir)
+
+            p = RawViscaPacket(1, 0, data)
+            handle_pan_tilt_packet(self.camera, p)
 
 if __name__ == '__main__':
     physicsClient = pb.connect(pb.GUI)
