8e6e687eb4088533ca003536a2b1e3a5eb3ec3dd
Maxime Desmarais <contact@mdesmarais.fr>
2021-11-10 10:52:15 +0100

    Add install script

---
+++ setup.py
@@ -0,0 +1,24 @@
+import os
+
+from setuptools import setup
+
+deps = []
+requirements_path = os.path.join(os.path.realpath(os.path.dirname(__file__)), 'requirements.txt')
+with open(requirements_path, 'r') as f:
+    for line in f:
+        if len(line.strip()) > 0:
+            deps.append(line.strip())
+
+
+setup(
+    name='ptzpicocam',
+    description='Simlation of a ptz camera',
+    author='Sylvain Courty, Maxime Desmarai',
+    author_email='sylvain.courty@apside-groupe.com, maxime.desmarais@apside-groupe.com',
+    packages=['ptzpicocam', 'ptzsimcam'],
+    package_data={
+        'ptzsimcam': ['*.urdf']
+    },
+    install_requires=deps,
+    scripts=['scripts/ptzsim']
+)
